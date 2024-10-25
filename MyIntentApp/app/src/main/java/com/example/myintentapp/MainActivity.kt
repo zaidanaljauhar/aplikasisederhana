f4f86c08497f39893191b92105891165c87aad5b
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2024-10-25 17:03:48 +0700

    tugas my intens app

---
+++ MyIntentApp/app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myintentapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnMoveActivity = findViewById<Button>(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener {
+            Onclick()
+        }
+    }
+
+    private fun Onclick() {
+        val intent = Intent(applicationContext, MoveActivity::class.java)
+        startActivity(intent)
+    }
+}
